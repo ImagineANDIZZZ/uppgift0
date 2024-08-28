6f056613d43c2ff6e4dcf3789c4311a56a85633c
ImagineANDIZZZ <Arwi07001@utb.vaxjo.se>
2024-08-28 11:16:51 +0200

    uppgift0

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("säg två tal");
+string text=Console.ReadLine();
+string text2=Console.ReadLine();
+int tal=int.Parse(text);
+int tal2=int.Parse(text2);
+Console.WriteLine(tal + tal2);
+Console.WriteLine(tal - tal2);
---
+++ namn+efternamn.cs
@@ -0,0 +1,4 @@
+Console.WriteLine("namn+efternamn");
+string text=Console.ReadLine();
+string text2=Console.ReadLine();
+Console.WriteLine(text + text2);
